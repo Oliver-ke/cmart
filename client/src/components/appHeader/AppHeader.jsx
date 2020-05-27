fd905e5e223ce9ebe5ad4c42e16cfdfd69aa1149
Azorji Kelechi Oliver <kelechioliver96@gmail.com>
2020-05-27 12:18:25 +0100

    add project file structure

---
+++ client/src/components/appHeader/AppHeader.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+import '.appHeader.style.css'
+const AppHeader = () => {
+  return (
+    <div className="app-header">
+      <div className="app-logo">
+        <img />
+      </div>
+    </div>
+  )
+}
+
+export default AppHeader;
